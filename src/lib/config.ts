420dea0c1a666485588e3776737399e823fd0ed2
Igor <86168299+SpaceIgor@users.noreply.github.com>
2024-08-12 18:32:59 +0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -11,18 +11,33 @@ export const config = {
   },
   texts: {
     about:
-      "Hi, I'm Ihor, a Python developer with a strong understanding of the language. Recognized as the Person of the Year in 2006, I excel at problem-solving and delivering high-quality, maintainable code. I'm passionate about collaborating with a team to create innovative Python solutions.",
-    contact:
+      "Hi, I'm Ihor, a Python developer with a deep understanding of the language and a proven track record of delivering high-quality, maintainable code. Recognized as the Person of the Year in 2006, I excel in problem-solving and thrive in collaborative environments, where I work closely with teams to create innovative and efficient Python solutions. My experience spans working with microservices, optimizing code performance, and mentoring junior developers. I am passionate about continuous learning and applying cutting-edge technologies to solve complex challenges.",    
+      contact:
       "I am waiting for new challenges, my mail is always open for any questions. Just ask anything and I will do my best to reach you with answers!",
   },
   experience: [
     {
-      title: "Ninja Taxi",
-      time_interval: "Feb 2024 - Sep 2022",
+      title: "InDevLab",
+      time_interval: "Fev 2024 - Present",
       responsibilities: [
-        "Create tasks for junior and intern. Mentoring",
-        "Writing clean, efficient, and maintainable code",
-        "Troubleshooting, debugging, and resolving software defects and issues",
+        "Writing clean, efficient, and maintainable code.",
+        "Troubleshooting, debugging, and resolving software defects and issues.",
+        "Collaborating with other teams to ensure the integration of software solutions.",
+        "Participating in the design and planning of new features and components.",
+        "Working with various services based on microservices architecture.",
+        "Developing and integrating microservices to ensure system scalability and reliability.",
+        "Optimizing the interaction between microservices and ensuring their seamless operation.",
+      ],
+    },
+    {
+      title: "Ninja Taxi",
+      time_interval: "Sep 2022 - Feb 2024",
+      responsibilities: [
+        "Creating tasks for junior specialists and interns, mentoring.",
+        "Writing clean, efficient, and maintainable code.",
+        "Troubleshooting, debugging, and resolving software defects and issues.",
+        "Collaborating with other teams to ensure the integration of software solutions.",
+        "Participating in the design and planning of new features and components.",
       ],
     },
     {
@@ -31,15 +46,27 @@ export const config = {
       responsibilities: [
         "Writing clean, efficient, and maintainable code.",
         "Contributing to the improvement of development processes and practices.",
+        "Testing and verifying code to meet quality standards.",
+        "Documenting software modules and components.",
       ],
     },
     {
-      title: "SysAdmin",
+      title: "Hillel",
+      time_interval: "Jun 2021 - Sep 2021",
+      responsibilities: [
+        "Student, writing clean, efficient, and maintainable code.",
+        "Completing practical tasks and projects.",
+        "Learning the basics of programming and modern software development approaches.",
+      ],
+    },
+    {
+      title: "Feya Service",
       time_interval: "Nov 2018 - Apr 2019",
       responsibilities: [
         "Managed and maintained servers, networks, and infrastructure components.",
         "Monitored system performance and ensured optimal operation.",
         "Installed, configured, and updated software and hardware systems.",
+        "Provided technical support and resolved user issues."
       ],
     },
   ],
