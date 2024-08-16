126a040211b7a2a9048177e4497a163096e4570a
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-08-16 10:53:01 +0300

    fix: padding for education section

--- src/components/sections/education.tsx
+++ src/components/sections/education.tsx
@@ -6,7 +6,7 @@ export const Education = () => {
   return (
     <section
       id="education"
-      className="container min-h-screen flex justify-center items-center"
+      className="container min-h-screen flex justify-center items-center pt-20 sm:pt-10 md:pt-0"
     >
       <div className="w-full flex flex-col gap-4 md:gap-8 md:w-3/5">
         <h2 className="text-neon text-4xl md:text-5xl">Education</h2>
@@ -20,7 +20,9 @@ export const Education = () => {
                 <span className="text-neon text-xl">
                   {item.degree}: {item.time_interval}
                 </span>
-                <p className=" text-primary text-xl bg-neon-light bg-clip-text">{item.description}</p>
+                <p className=" text-primary text-xl bg-neon-light bg-clip-text">
+                  {item.description}
+                </p>
               </li>
             ))}
           </ul>
